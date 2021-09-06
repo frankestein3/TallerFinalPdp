27e1ab799631c09ff6c700592b3af8f0e7141a7a
Juan Felipe Velasco García <juan_velasco82161@elpoli.edu.co>
2021-09-06 12:42:54 -0500

    css y cuenta regresiva

--- src/componentes/game/Game.jsx
+++ src/componentes/game/Game.jsx
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect, useRef } from "react";
 import { useParams } from "react-router";
 import "./game.css";
 import { Modal, Button, Form } from "react-bootstrap";
@@ -8,30 +8,47 @@ const Game = () => {
   const [indice, setIndice] = useState(0)
   const [ganancia, setGanancia] = useState(0)
   const [modal, setModal] = useState(false)
+  let intervalRef = useRef();
+  const decreaseNum = () => {
+    if (num > 0) {
+      setNum((prev) => prev - 1);
+      console.log(num);
+    } else {
+      console.log(num);
+    }
+  };
+  const [num, setNum] = useState(30);
 
-        useEffect(() => {
-            fetch(
-            `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficult}&type=multiple`
-            )
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data.results);
-                setPreguntas(data.results);
-            });
-        }, []);
+  useEffect(() => {
 
-        const { user } = useParams();
-        const { category } = useParams();
-        const { difficult } = useParams();
+    fetch(
+      `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficult}&type=multiple`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setPreguntas(data.results);
+      });
 
-        const handleClose = () => setModal(false);
-        const cambios = () => 
-        {setIndice(indice+1);
-         setGanancia(ganancia+1000);
-         if(indice == 9){
-             setModal(true);
-         }
-        };
+    intervalRef.current = setInterval(decreaseNum, 1000);
+
+    //return () => clearInterval(intervalRef.current);
+
+  }, []);
+
+  const { user } = useParams();
+  const { category } = useParams();
+  const { difficult } = useParams();
+
+  const handleClose = () => setModal(false);
+  const cambios = () => {
+    setNum(30);
+    setIndice(indice + 1);
+    setGanancia(ganancia + 1000);
+    if (indice == 9) {
+      setModal(true);
+    }
+  };
 
   return (
     <div>
@@ -51,52 +68,51 @@ const Game = () => {
         </div>
       </nav>
       <br></br>
-      <div className="card">
+      <div className="card_game">
         <br></br>
-        <button class="counter">30</button>
-        <br></br>
-        <p>{preguntas[indice] ? preguntas[indice].question :"" }</p>
+        <button class="counter">{(num >= 0) ? num : 0}</button>
+        <br></br><br />
+        <p>{preguntas[indice] ? preguntas[indice].question : ""}</p>
         <br></br>
         <button class="btn btn-dark btn-lg btn-block optionsbtn" onClick={cambios}>
-        {preguntas[indice] ? preguntas[indice].correct_answer :""}
+          {preguntas[indice] ? preguntas[indice].correct_answer : ""}
         </button>
-        <br></br>
+        <br></br><br />
         <button class="btn btn-dark btn-lg btn-block optionsbtn">
-        {preguntas[indice] ? preguntas[indice].incorrect_answers[0] :"" }
+          {preguntas[indice] ? preguntas[indice].incorrect_answers[0] : ""}
         </button>
-        <br></br>
+        <br></br><br />
         <button class="btn btn-dark btn-lg btn-block optionsbtn">
-        {preguntas[indice] ? preguntas[indice].incorrect_answers[1] :"" }
+          {preguntas[indice] ? preguntas[indice].incorrect_answers[1] : ""}
         </button>
-        <br></br>
+        <br></br><br />
         <button class="btn btn-dark btn-lg btn-block optionsbtn">
-        {preguntas[indice] ? preguntas[indice].incorrect_answers[2] :"" }
+          {preguntas[indice] ? preguntas[indice].incorrect_answers[2] : ""}
         </button>
       </div>
-      <div className="card winning">
-        <p>1 $1,000</p>
-        <p>2 $2,000</p>
-        <p>3 $3,000</p>
-        <p>4 $4,000</p>
-        <p>5 $5,000</p>
-        <p>6 $6,000</p>
-        <p>7 $7,000</p>
-        <p>8 $8,000</p>
-        <p>9 $9,000</p>
-        <p>10 $10,000</p>
+      <div className="card_winning">
+        <p>10. $10,000</p>
+        <p>9. $9,000</p>
+        <p>8. $8,000</p>
+        <p>7. $7,000</p>
+        <p>6. $6,000</p>
+        <p>5. $5,000</p>
+        <p>4. $4,000</p>
+        <p>3. $3,000</p>
+        <p>2. $2,000</p>
+        <p>1. $1,000</p>
       </div>
       <Modal show={modal} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>¡GANASTE!</Modal.Title>
-          </Modal.Header>
-          <Modal.Footer>
-            <Button onClick={(() => window.location="/")}>
-              Ok
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <Modal.Header closeButton>
+          <Modal.Title>¡GANASTE!</Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button onClick={(() => window.location = "/")}>
+            Ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
-    
   );
 };
 
--- src/componentes/home/Home.js
+++ src/componentes/home/Home.js
@@ -1,91 +1,95 @@
-import React, {Fragment, useState}from 'react';
+import React, { Fragment, useState } from 'react';
 import { Component } from 'react';
-import {Link } from "react-router-dom";
-import './Home.css';  
+import { Link } from "react-router-dom";
+import './Home.css';
 import { useHistory } from "react-router-dom";
 
-const Home  = () => {
+const Home = () => {
 
-        const [datos, setDatos] = useState({
-            usuario: '',
-            categoria: '',
-            dificultad: ''
+    const [datos, setDatos] = useState({
+        usuario: '',
+        categoria: '',
+        dificultad: ''
+    })
+
+    const handleInputChange = (event) => {
+        //console.log(event.target.value)
+        setDatos({
+            ...datos,
+            [event.target.name]: event.target.value
+
         })
+    }
+    const enviarDatos = (event) => {
+        event.preventDefault();
+        let user = datos.usuario
+        let category = datos.categoria
+        let difficult = datos.dificultad
+        /*console.log(datos.usuario);
+        console.log(datos.categoria);
+        console.log(datos.dificultad);*/
+        history.push(`/Home/Game/${user}/${category}/${difficult}`);
+    }
 
-       
+    let history = useHistory();
 
-        const handleInputChange = (event) =>{
-            //console.log(event.target.value)
-            setDatos({
-                ...datos,
-                [event.target.name] : event.target.value
-                
-            })
-        }
-        const enviarDatos = (event) => {
-            event.preventDefault();
-            let user = datos.usuario
-            let category = datos.categoria
-            let difficult = datos.dificultad
-            /*console.log(datos.usuario);
-            console.log(datos.categoria);
-            console.log(datos.dificultad);*/
-            history.push(`/Home/Game/${user}/${category}/${difficult}`);
-        }
-        
-        let history = useHistory();
-
-        
-
-       
-        return (
-            <Fragment>
-                <main role="main" className="flex-shrink-0 mt-5">
-                    <div className="card">
-                    <h1> ¿QUIEN QUIERE SER MILLONARIO? </h1>
+    return (
+        <Fragment>
+            <main role="main" className="flex-shrink-0 mt-5">
+                <div className="card">
+                    <h1> ¿QUIÉN QUIERE SER MILLONARIO? </h1>
                     <form onSubmit={enviarDatos}>
-                    <div class="form-group">
-                        <label>Usuario</label>
-                        <input type="text" name="usuario" class="form-control" id="usuario" placeholder="Ingrese nombre"
-                        onChange={handleInputChange} required
-                        />
-                    </div>
-                    <br></br>
-                    <div class="form-group">
-                        <label for="selectCategory">Categoria</label>
-                            <select class="form-control" id="categoria"  name="categoria" onChange={handleInputChange} required>
-                                <option value="">Seleccionar Categoria</option>
+                        <div class="form-group">
+                            <br />
+                            <br />
+                            <label>Usuario</label>
+                            <input type="text" name="usuario" class="form-control" id="usuario" placeholder="Ingrese nombre"
+                                onChange={handleInputChange} required
+                            />
+                        </div>
+                        <br></br>
+                        <div class="form-group">
+                            <label for="selectCategory">Categoría</label>
+                            <select class="form-control" id="categoria" name="categoria" onChange={handleInputChange} required>
+                                <option value="">Seleccionar Categoría</option>
                                 <option value="21">Deporte</option>
                                 <option value="27">Animales</option>
                                 <option value="25">Arte</option>
-                                <option value="20" >Mitologia</option>
-                                <option value="22" >Geografia</option>
-                            </select>  
-                    </div>
-                    <br></br>
-                    <div class="form-group">
-                        <label for="selectCategory">Dificultad</label>
-                            <select class="form-control" id="dificultad"  name="dificultad" onChange={handleInputChange} required>
+                                <option value="20" >Mitología</option>
+                                <option value="22" >Geografía</option>
+                            </select>
+                        </div>
+                        <br></br>
+                        <div class="form-group">
+                            <label for="selectCategory">Dificultad</label>
+                            <select class="form-control" id="dificultad" name="dificultad" onChange={handleInputChange} required>
                                 <option value="">Seleccionar Dificultad</option>
                                 <option value="easy">Facil</option>
                                 <option value="medium">Normal</option>
                                 <option value="hard">Dificil</option>
-                            </select>  
-                    </div>
-                    <br></br>
-                    <button type="submit" class="btn btn-dark" >
-                        Ingresar
-                    </button>
+                            </select>
+                        </div>
+                        <br></br><br />
+                        <button type="submit" class="btn btn-dark">
+                            Ingresar
+                        </button>
 
-                    <br></br>
-                    </form> 
-                    </div>  
-                </main>  
-            </Fragment>  
-        )
-      
-  
-   
+                        <br></br>
+                    </form>
+                </div>
+            </main>
+            <br />
+            <footer>
+                <div className="container">
+                    <h5>Desarrollado por:</h5>
+                    <br />
+                    <h5>Juan Pablo Hincapie Cardenas</h5>
+                    <h5>Valentina Hernandez Arroyave</h5>
+                </div>
+
+            </footer>
+        </Fragment>
+    )
 };
 
-  export default Home;
+export default Home;
